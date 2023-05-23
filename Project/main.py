2b7985c5075d70dab65cfa62a07850e5814db794
Auren Smith <270174859@yoobeestudent.ac.nz>
2023-05-23 16:28:42 +1200

    start of cards dropdown

--- Project/main.py
+++ Project/main.py
@@ -1,45 +1,77 @@
-# This Python file uses the following encoding: utf-8
+# this Python file uses the following encoding: utf-8
 import sys
 from pathlib import Path
 
-from PySide6.QtCore import QFile
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
+from PySide6.QtCore import QFile, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFrame, QVBoxLayout, QHBoxLayout, QMenu
 
-if __name__ == "__main__":
+import variables as var
+
+if __name__ == "__main__":    
+    # create application
     app = QApplication(sys.argv)
 
-    # Create the main window
+    # create the main window
     window = QMainWindow()
     window.setWindowTitle("Yoobee Project")
     window.resize(375, 812)
 
-    # Load the stylesheet file
+    # load the stylesheet file
     style_file = QFile(str(Path("style.qss")))
     style_file.open(QFile.ReadOnly | QFile.Text)
 
-    # Apply the stylesheet to the application
+    # apply the stylesheet to the application
     app.setStyleSheet(style_file.readAll().data().decode())
 
-    # Add widgets to the main window
+    # add widgets to the main window
+
+    # sidebar button
     sidebarButton = QPushButton("Sidebar", window)
-    sidebarButton.setGeometry(23, 39, 61, 61)
+    #sidebarButton.setGeometry(23, 39, 61, 61)
     sidebarButton.setObjectName("sidebarButton")
 
+    # live chat button
     liveButton = QPushButton("Live Chat", window)
     liveButton.setGeometry(289, 39, 61, 61)
     liveButton.setObjectName("liveButton")
 
-    cards = QPushButton("", window)
-    cards.setGeometry(25, 175, 325, 200)
-    cards.setObjectName("cards")
+    # add new cards button
+    #cards = QPushButton("", window)
+    #cards.setGeometry(25, 175, 325, 200)
+    #cards.setObjectName("cards")
 
-    plus = QPushButton("+", cards)
-    plus.setGeometry(137, 75, 50, 50)
-    plus.setObjectName("plusCircle")
+    # circle plus button on cards
+    #plus = QPushButton("+", cards)
+    #plus.setGeometry(137, 75, 50, 50)
+    #plus.setObjectName("plus")
 
-    # Load the main window as the root object
+    menu = QPushButton("", window)
+    menu.setGeometry(25, 175, 325, 200)
+    menu.setObjectName("noBorder")
+
+    menuBorder = QPushButton("", window)
+    menuBorder.setGeometry(25, 175, 325, 200)
+    menuBorder.setObjectName("noBorder")
+
+    # load the main window as the root object
     window.show()
 
-    # Code
+    # code
+    sidebarButton.clicked.connect(var.buttonPressed)
+
+    menuOpen = False
+    def menuPressed():
+        global menuOpen
+        menuOpen = not menuOpen
+        if menuOpen:
+            menu.setGeometry(25, 175, 325, 592)
+            menuBorder.setStyleSheet("border: 5px solid #545454;")
+            menuBorder.setGeometry(20, 170, 335, 210)
+        else:
+            menu.setGeometry(25, 175, 325, 200)
+            menuBorder.setStyleSheet("border: none;")
+            menuBorder.setGeometry(25, 175, 325, 200)
+
+    menuBorder.clicked.connect(menuPressed)
 
     sys.exit(app.exec())
---
+++ Project/variables.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+
+myBool = False
+def buttonPressed():
+    global myBool
+    myBool = not myBool
+    if myBool:
+        print(myBool)
+    else:
+        print(myBool)
